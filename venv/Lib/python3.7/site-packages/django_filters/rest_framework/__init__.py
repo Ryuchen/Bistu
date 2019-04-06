aae75f66a946c5f032aaf8947c848c7097bd6c5b
chenhao <chenhao@geyecloud.com>
2019-04-06 16:16:41 +0800

    remove unnecessary file

--- venv/Lib/python3.7/site-packages/django_filters/rest_framework/__init__.py
+++
@@ -1,4 +0,0 @@
-# flake8: noqa
-from .backends import DjangoFilterBackend
-from .filterset import FilterSet
-from .filters import *
--- venv/Lib/site-packages/rest_framework_bulk/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = '0.2.1'
-__author__ = 'Miroslav Shubernetskiy'
-
-try:
-    from .generics import *  # noqa
-    from .mixins import *  # noqa
-    from .serializers import *  # noqa
-except Exception:
-    pass
