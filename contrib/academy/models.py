201a241b0822dc14fa2832669c4aa31826e1b2b9
chenhao <chenhao@geyecloud.com>
2019-04-06 17:03:27 +0800

    rebuild models

--- contrib/academy/models.py
+++ contrib/academy/models.py
@@ -9,17 +9,41 @@
 # ==================================================
 import uuid
 
+from core.definition.enums import *
+
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Research(models.Model):
+    """
+    科研方向模型
+    """
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, help_text="唯一标识ID")
+    res_name = models.CharField(max_length=128, null=True, help_text="研究方向")
+
+    def __str__(self):
+        return self.res_name
+
+    class Meta:
+        verbose_name = "研究方向"
+        verbose_name_plural = verbose_name
 
 
 class Major(models.Model):
     """
-    学科专业模型
+    学科模型
     """
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, help_text="唯一标识ID")
-    maj_name = models.CharField(max_length=128, null=True, help_text="学科专业名称")
-    maj_code = models.IntegerField(null=True, help_text="学科专业编号")
-    maj_type = models.CharField(max_length=128, null=True)
+    maj_name = models.CharField(max_length=128, null=True, help_text="学科名称")
+    maj_code = models.IntegerField(null=True, help_text="学科编号")
+    maj_type = models.CharField(max_length=128, choices=[(tag.name, tag.value) for tag in MajorType], help_text="学科类型")
+    maj_first = models.BooleanField(help_text="是否一级学科")
+    maj_second = models.BooleanField(help_text="是否二级学科")
+    maj_first_uuid = models.UUIDField(help_text="所属一级学科")
+    maj_setup_time = models.DateField(help_text="获批时间")
+    maj_degree = models.CharField(max_length=128, choices=[(tag.name, tag.value) for tag in MajorDegree], help_text="学位类型")
+    research = models.ManyToManyField(Research, help_text="科研方向")
 
     def __str__(self):
         return str(self.maj_code) + self.maj_name
@@ -34,12 +58,20 @@ class Academy(models.Model):
     学院模型
     """
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, help_text="唯一标识ID")
-    aca_name = models.CharField(max_length=128, null=True, help_text="学院名称")
+    aca_avatar = models.ImageField(help_text="学院图标")
+    aca_nickname = models.CharField(max_length=128, null=True, help_text="学院简称")
+    aca_cname = models.CharField(max_length=128, null=True, help_text="学院名称(中)")
+    aca_ename = models.CharField(max_length=128, null=True, help_text="学院名称(英)")
     aca_code = models.IntegerField(null=True, help_text="学院代码")
+    aca_phone = models.CharField(max_length=128, null=True, help_text="学院电话")
+    aca_fax = models.CharField(max_length=128, null=True, help_text="学院传真")
+    aca_href = models.URLField(max_length=256, null=True, help_text="学院网址")
+    aca_brief = models.TextField(help_text="学院简介")
+    aca_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, help_text="学院负责人")
     majors = models.ManyToManyField(Major, related_name='majors')
 
     def __str__(self):
-        return str(self.aca_code) + self.aca_name
+        return str(self.aca_code) + self.aca_cname
 
     class Meta:
         verbose_name = "学院"
--- contrib/users/models.py
+++ contrib/users/models.py
@@ -11,7 +11,7 @@ import uuid
 
 from core.definition.enums import *
 
-from contrib.academy.models import Academy, Major
+from contrib.academy.models import Academy, Major, Research
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -26,6 +26,7 @@ class Education(models.Model):
     edu_finish_time = models.DateField(help_text="结束时间")
     edu_school_name = models.CharField(max_length=128, null=True, help_text="学校名称")
     edu_study_major = models.CharField(max_length=128, null=True, help_text="专业方向")
+    edu_study_field = models.CharField(max_length=128, null=True, help_text="研究领域")
 
     def __str__(self):
         return "学校：{0}  专业：{1}".format(self.edu_school_name, self.edu_study_major)
@@ -66,15 +67,16 @@ class Tutor(models.Model):
     tut_political = models.CharField(max_length=64, choices=sorted([(tag.name, tag.value) for tag in PoliticalChoice]), help_text="政治面貌")
     tut_telephone = models.IntegerField(null=True, help_text="电话号码")
     tut_degree = models.CharField(max_length=64, choices=sorted([(tag.name, tag.value) for tag in DegreeChoice]), help_text="学位")
-    education = models.OneToOneField(Education, null=True, on_delete=True, related_name='education', help_text="学历")
+    education = models.ForeignKey(Education, null=True, on_delete=True, related_name='education', help_text="学历")
     academy = models.ForeignKey(Academy, null=True, on_delete=models.CASCADE, related_name='academy', help_text="所属学院")
 
     def __str__(self):
-        return "工号：{0}  姓名：{1}".format(self.tut_number, self.user.last_name + self.user.first_name)
+        return "工号：{0}  姓名：{1}".format(self.tut_number, self.user.first_name + self.user.last_name)
 
     class Meta:
         verbose_name = "导师"
         verbose_name_plural = verbose_name
+        ordering = ['tut_number']
 
 
 class Student(models.Model):
@@ -83,9 +85,9 @@ class Student(models.Model):
     """
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, help_text="唯一标识ID")
     user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name='stu_user')
-    stu_number = models.IntegerField(null=False, unique=True, help_text="学号", default='001')
-    stu_gender = models.CharField(max_length=64, null=True, choices=[(tag.name, tag.value) for tag in GenderChoice],
-                                  help_text="性别")
+    stu_number = models.IntegerField(null=False, unique=True, default='20190101', help_text="学号")
+    stu_avatar = models.ImageField(null=True, help_text="学生照片")
+    stu_gender = models.CharField(max_length=64, null=True, choices=[(tag.name, tag.value) for tag in GenderChoice], help_text="性别")
     stu_card_type = models.CharField(max_length=128, null=False, help_text='身份证件类型', default="身份证")
     stu_cardID = models.CharField(max_length=128, null=True, unique=True, help_text="身份证号", default="12345")
     stu_candidate_number = models.CharField(max_length=128, null=True, help_text="考生号")
@@ -93,38 +95,29 @@ class Student(models.Model):
     stu_nation = models.CharField(max_length=64, null=False, help_text='民族', default='汉')
     stu_source = models.CharField(max_length=128, null=True, help_text="生源地")
     stu_is_village = models.BooleanField(default=False, help_text='是否农村学生')
-    stu_political = models.CharField(max_length=64, null=True,
-                                     choices=[(tag.name, tag.value) for tag in PoliticalChoice], help_text="政治面貌")
-    stu_type = models.CharField(max_length=128, null=False, choices=[(tag.name, tag.value) for tag in StudentType],
-                                help_text='学生类型', default='S1')
-    stu_learn_type = models.CharField(max_length=64, null=False, choices=[(tag.name, tag.value) for tag in StudentCategory],
-                                      help_text='学习形式', default='S1')
-    stu_learn_status = models.CharField(max_length=64, null=False, help_text='学习阶段', default='D2',
-                                        choices=[(tag.name, tag.value) for tag in DegreeChoice])
+    stu_political = models.CharField(max_length=64, null=True, choices=[(tag.name, tag.value) for tag in PoliticalChoice], help_text="政治面貌")
+    stu_type = models.CharField(max_length=128, null=False, choices=[(tag.name, tag.value) for tag in StudentType], help_text='学生类型', default='S1')
+    stu_learn_type = models.CharField(max_length=64, null=False, choices=[(tag.name, tag.value) for tag in StudentCategory], help_text='学习形式', default='S1')
+    stu_learn_status = models.CharField(max_length=64, null=False, help_text='学习阶段', default='D2', choices=[(tag.name, tag.value) for tag in DegreeChoice])
     stu_grade = models.CharField(max_length=64, null=False, help_text='年级', default='1')
     stu_system = models.IntegerField(null=False, help_text='学制', default=3)
     stu_entrance_time = models.CharField(max_length=32, null=False, help_text='入学日期', default='2019-09')
     stu_graduation_time = models.CharField(max_length=32, null=False, help_text='毕业日期', default='2021-07')
-    stu_cultivating_mode = models.CharField(max_length=128, null=False, help_text='培养方式', default='C1',
-                                            choices=[(tag.name, tag.value) for tag in CultivatingMode])
-    stu_enrollment_category = models.CharField(max_length=64, null=False,
-                                               choices=[(tag.name, tag.value) for tag in EnrollmentCategory],
-                                               help_text='录取类别', default='E1')
-    stu_natioanlity = models.CharField(max_length=128, null=False, help_text='国籍', default='中国')
-    stu_special_program = models.CharField(max_length=128, null=False,
-                                           choices=[(tag.name, tag.value) for tag in SpecialProgram],
-                                           help_text='专项计划', default='S1')
+    stu_cultivating_mode = models.CharField(max_length=128, null=False, help_text='培养方式', default='C1', choices=[(tag.name, tag.value) for tag in CultivatingMode])
+    stu_enrollment_category = models.CharField(max_length=64, null=False, choices=[(tag.name, tag.value) for tag in EnrollmentCategory], help_text='录取类别', default='E1')
+    stu_nationality = models.CharField(max_length=128, null=False, help_text='国籍', default='中国')
+    stu_special_program = models.CharField(max_length=128, null=False, choices=[(tag.name, tag.value) for tag in SpecialProgram], help_text='专项计划', default='S1')
     stu_is_regular_income = models.BooleanField(default=False, help_text='是否有固定收入')
     stu_is_tuition_fees = models.BooleanField(default=False, help_text='是否欠缴学费')
-    stu_is_achives = models.BooleanField(default=False, help_text='档案是否转到学校')
-    stu_telephone = models.IntegerField(null=True, help_text="电话号码")
-    stu_status = models.CharField(max_length=64, null=False, choices=[(tag.name, tag.value) for tag in StatusChoice],
-                                  help_text="在学状态", default='S1')
+    stu_is_archives = models.BooleanField(default=False, help_text='档案是否转到学校')
     stu_is_superb = models.BooleanField(default=False, help_text="是否优秀毕业生")
+    stu_telephone = models.IntegerField(null=True, help_text="电话号码")
+    stu_status = models.CharField(max_length=64, null=False, choices=[(tag.name, tag.value) for tag in StatusChoice], help_text="在学状态", default='S1')
     stu_class = models.CharField(max_length=128, null=True, help_text="所属班级")
-    academy = models.ForeignKey(Academy, null=True, on_delete=models.SET_NULL, related_name='stu_academy', help_text='所属学院')
     tutor = models.ForeignKey(Tutor, null=True, related_name='stu_tutor', on_delete=models.SET_NULL, help_text="指导老师")
+    academy = models.ForeignKey(Academy, null=True, related_name='stu_academy', on_delete=models.SET_NULL, help_text='所属学院')
     major = models.ForeignKey(Major, null=True, related_name='stu_major', on_delete=models.SET_NULL, help_text="学科专业")
+    research = models.ForeignKey(Research, null=True, related_name='stu_research', on_delete=models.SET_NULL, help_text="科研方向")
 
     def __str__(self):
         self.stu_gender = self.get_stu_gender_display()
@@ -136,7 +129,7 @@ class Student(models.Model):
         self.stu_enrollment_category = self.get_stu_enrollment_category_display()
         self.stu_special_program = self.get_stu_special_program_display()
         self.stu_status = self.get_stu_status_display()
-        return "学生编号：{0}     学生姓名：{1}".format(self.stu_number, self.user.last_name + self.user.first_name)
+        return "学生编号：{0}     学生姓名：{1}".format(self.stu_number, self.user.first_name + self.user.last_name)
 
     class Meta:
         verbose_name = "学生"
