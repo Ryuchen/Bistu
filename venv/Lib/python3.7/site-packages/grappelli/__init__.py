07890ee3cc160adc3ace10e14b202d1968685488
chenhao <chenhao@geyecloud.com>
2019-04-06 09:58:25 +0800

    push

---
+++ venv/Lib/python3.7/site-packages/grappelli/__init__.py
@@ -0,0 +1,2 @@
+VERSION = '2.12.2'
+default_app_config = 'grappelli.apps.GrappelliConfig'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ryuchen/PycharmProjects/Bistu/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
