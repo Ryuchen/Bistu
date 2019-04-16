8936e8d374186f9b5d398684a0cb8bfe63a5f258
libin <libin@geyeclouds.com>
2019-04-16 19:33:16 +0800

    merger

---
+++ apps/common/views.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ==================================================
+# @Time : 2019-04-16 17:05 
+# @Author : libin
+# @Site :  
+# @File : views.py 
+# @Desc : 
+# ==================================================
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
+
+from core.definition.enums import *
+
+
+@api_view(["GET"])
+@csrf_exempt
+def common_settings(request):
+	res = dict()
+	res['degree_choice'] = {tag.name: tag.value for tag in DegreeChoice}
+	res['gender_choice'] = {tag.name: tag.value for tag in GenderChoice}
+	res['title_choice'] = {tag.name: tag.value for tag in TitleChoice}
+	res['political_choice'] = {tag.name: tag.value for tag in PoliticalChoice}
+	res['status_choice'] = {tag.name: tag.value for tag in StatusChoice}
+	res['student_type'] = {tag.name: tag.value for tag in StudentType}
+	res['student_category'] = {tag.name: tag.value for tag in StudentCategory}
+	res['cultivating_mode'] = {tag.name: tag.value for tag in CultivatingMode}
+	res['enrollment_category'] = {tag.name: tag.value for tag in EnrollmentCategory}
+	res['special_program'] = {tag.name: tag.value for tag in SpecialProgram}
+	res['major_type'] = {tag.name: tag.value for tag in MajorType}
+	res['major_degree'] = {tag.name: tag.value for tag in MajorDegree}
+	return Response(res)
