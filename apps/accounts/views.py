711b1403acfbbc88a7b2df6347e223ec38fd9f94
chenhao <chenhao@geyecloud.com>
2019-04-17 17:39:25 +0800

    update code to rebuild component

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -15,6 +15,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
 
+from contrib.academy.models import Academy
 from core.decorators.excepts import excepts
 from core.exceptions.errors import *
 
@@ -78,27 +79,30 @@ def current_user_view(request):
         raise NotImplementedError
 
     if request.user.is_authenticated:
-        if request.user.is_superuser:
+        user = User.objects.get(id=request.user.id)
+        if user.is_superuser:
             res["data"]["profile"] = {
-                "name": request.user.username,
-                "email": request.user.email,
+                "name": user.username,
+                "email": user.email,
                 "avatar": 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
                 "title": "你是当前系统的最高管理员",
                 "group": "超级管理员"
             }
         else:
-            if request.user.is_staff:
+            if user.is_staff:
+                academy = Academy.objects.filter(aca_user_id=user.id).first()
                 res["data"]["profile"] = {
-                    "name": request.user.username,
-                    "email": request.user.email,
+                    "name": user.username,
+                    "email": user.email,
                     "avatar": 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
-                    "title": "你是{0}学院的管理员".format(request.user.academy.aca_cname),
-                    "group": "学院管理人员"
+                    "title": "你是{0}学院的管理员".format(academy.aca_cname),
+                    "group": "学院管理人员",
+                    "academy": academy.uuid
                 }
             else:
                 res["data"]["profile"] = {
-                    "name": request.user.username,
-                    "email": request.user.email,
+                    "name": user.username,
+                    "email": user.email,
                     "avatar": 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
                     "title": "研究生导师",
                     "group": "教师"
