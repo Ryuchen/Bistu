58c41890a6be4cfc4db299c69cb774d3642bc6fa
chenhao <chenhao@geyecloud.com>
2019-06-11 20:23:29 +0800

    add line chart table

--- apps/students/urls.py
+++ apps/students/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
 	url(r"^student/(?P<pk>[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12})$", StudentDetail.as_view(), name="student-detail"),
 	url(r"^statistics/$", StudentStatistics.as_view(), name="student-statistics"),
 	url(r"^create_xls/$", views.create_xls, name="create_xls"),
+	url(r"^trending/$", views.academies_enroll_statistic, name="student-enroll-trending"),
 ]
