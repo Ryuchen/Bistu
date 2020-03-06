ed71ecd139acea41e858e0ed4da84d0700e8df99
Ryuchen <chenhaom1993@hotmail.com>
2020-03-06 13:57:49 +0800

    fix some issues

---
+++ contrib/accounts/auth/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ==================================================
+# @Time : 2020/3/6-11:30
+# @Author : Ryuchen
+# @Site : https://ryuchen.github.io
+# @File : __init__.py
+# @Desc : 
+# ==================================================
